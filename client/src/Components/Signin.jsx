2a93d85c84eaefaab83349a80088cc9bbcaca4ee
nikumbhShubham <shubhamnikumbh19@gmail.com>
2024-04-07 02:49:41 +0530

    some changes

--- client/src/Components/Signin.jsx
+++ client/src/Components/Signin.jsx
@@ -8,27 +8,28 @@ const Signin = () => {
 
     return (
         <>
-            <div className="w-full bg-gray-200 h-screen text-black bo">
-                <nav className='w-full p-7 px-14 flex flex-row justify-between items-center'>
-                    <h1 className='text-md md:text-3xl'>
-                        <Link to="/"> Edu-Sphere</Link>
-                    </h1>
-                    {signIn ? <h1 className='text-sm sm:text-xl'>Already have an account?
-                        <span className='text-indigo-600 px-4 cursor-pointer' onClick={() => setSignIn(false)}>
-                            Create Account
-                        </span>
-                    </h1>
-                        :
-                        <h1 className='text-sm sm:text-xl'>Have an account?
-                            <span className='text-indigo-500 px-4 cursor-pointer' onClick={() => setSignIn(true)}>
-                                Sign In
-                            </span>
-                        </h1>}
-                </nav>
-                {
-                    signIn ? <Login /> : <CreateAccount />
-                }
-            </div>
+           <div className="w-full my-back bg-opacity-50 h-screen text-black">
+  <nav className='w-full p-7 px-14 flex flex-row justify-between items-center'>
+    <h1 className='text-md md:text-3xl'>
+      <Link to="/"> Edu-Sphere</Link>
+    </h1>
+    {signIn ? <h1 className='text-sm sm:text-xl'>Already have an account?
+      <span className='text-indigo-600 px-4 cursor-pointer' onClick={() => setSignIn(false)}>
+        Create Account
+      </span>
+    </h1>
+      :
+      <h1 className='text-sm sm:text-xl'>Have an account?
+        <span className='text-indigo-500 px-4 cursor-pointer' onClick={() => setSignIn(true)}>
+          Sign In
+        </span>
+      </h1>}
+  </nav>
+  {
+    signIn ? <Login /> : <CreateAccount />
+  }
+</div>
+
         </>
     )
 }
