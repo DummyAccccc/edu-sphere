821510b9e3f5bcf046481fb86ff3d8707f37c6e5
nikumbhShubham <shubhamnikumbh19@gmail.com>
2024-04-06 23:02:51 +0530

    hero02

--- client/src/Components/Hero02.jsx
+++ client/src/Components/Hero02.jsx
@@ -27,33 +27,33 @@ const Hero02 = () => {
 }
 
 export default Hero02;
-=======
-import React from 'react';
+
+// import React from 'react';
 
 
-const Hero02 = () => {
-  return (
-    <>
-    <div className="hero02-back text-white py-20 px-4 md:px-0">
-    <div className="container mx-auto max-w-6xl px-4">
-      <div className="flex flex-col md:flex-row items-center justify-between ">
-      <div className="md:w-1/2 mb-8 mr-20">
-          <img src="https://via.placeholder.com/500" alt="CRM Image" className="mx-auto md:ml-auto md:mb-8
+// const Hero02 = () => {
+//   return (
+//     <>
+//     <div className="hero02-back text-white py-20 px-4 md:px-0">
+//     <div className="container mx-auto max-w-6xl px-4">
+//       <div className="flex flex-col md:flex-row items-center justify-between ">
+//       <div className="md:w-1/2 mb-8 mr-20">
+//           <img src="https://via.placeholder.com/500" alt="CRM Image" className="mx-auto md:ml-auto md:mb-8
 
-           " />
-        </div>
-        <div className="md:w-1/2 mb-8 md:mb-0">
-          <h1 className="text-slate-800 text-6xl md:text-6xl  font-bold mb-4 md:mt-8">Streamline Your Education Platform</h1>
-          <p className="text-slate-800 text-lg md:text-xl mb-8">Empower your students and instructors with our comprehensive CRM solution.</p>
-          <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600">Get Started</button>
-        </div>
+//            " />
+//         </div>
+//         <div className="md:w-1/2 mb-8 md:mb-0">
+//           <h1 className="text-slate-800 text-6xl md:text-6xl  font-bold mb-4 md:mt-8">Streamline Your Education Platform</h1>
+//           <p className="text-slate-800 text-lg md:text-xl mb-8">Empower your students and instructors with our comprehensive CRM solution.</p>
+//           <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600">Get Started</button>
+//         </div>
         
-      </div>
-    </div>
-  </div>
-    </>
-  )
-}
+//       </div>
+//     </div>
+//   </div>
+//     </>
+//   )
+// }
 
-export default Hero02;
->>>>>>> 6ea7a5c1e14c891a3818c4300460bfaf88bf12df
+// export default Hero02;
+
