5341df02191c38d71f7d892b45eb0af48c93d232
Aditya G <adityagawali044@gmail.com>
2024-04-06 23:08:28 +0530

    changes

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 
@@ -29,35 +28,3 @@ const Hero = () => {
 }
 
 export default Hero;
-=======
-import React from 'react';
-
-
-const Hero = () => {
-    return (
-        <>
-            <div className=" text-slate-800 py-20 px-4 md:px-0">
-                <div className="container mx-auto max-w-6xl px-4">
-                    <div className="flex flex-col md:flex-row items-center justify-between">
-                        <div className="md:w-1/2 mb-8 md:mb-0">
-                            <h1 className="text-6xl md:text-6xl font-bold mb-4">Streamline Your Education Platform</h1>
-                            <p className="text-lg md:text-xl mb-8 mt-5">Empower your students and instructors with our comprehensive CRM solution.</p>
-                            <div className="flex flex-col md:flex-row">
-                                <div className="mb-4 md:mb-0 md:mr-4">
-                                    <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-700">Get Started</button>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div className="md:w-1/2">
-                            <img src="https://via.placeholder.com/500" alt="CRM Image" className="mx-auto md:ml-auto md:mr-0" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Hero;
->>>>>>> 6ea7a5c1e14c891a3818c4300460bfaf88bf12df
--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 import HomeNav from './HomeNav'
@@ -37,43 +36,3 @@ const HomePage = () => {
 }
 
 export default HomePage
-=======
-import React from 'react'
-
-import HomeNav from './HomeNav'
-import Hero from './Hero'
-import Hero02 from './Hero02'
-import Cards from './Cards'
-import Footer from './Footer'
-import FeaturesSection from './FeaturesSection'
-import UserGithub from '../Api/UserGithub'
-
-const HomePage = () => {
-    return (
-        <main >
-            <HomeNav />
-            <section>
-                <Hero />
-            </section>
-            <section>
-                <Hero02 />
-            </section>
-            <section>
-                <FeaturesSection />
-            </section>
-            <section >
-                <Cards />
-            </section>
-            <section >
-                <Footer />
-            </section>
-            <UserGithub />
-            
-
-
-        </main>
-    )
-}
-
-export default HomePage
->>>>>>> 6ea7a5c1e14c891a3818c4300460bfaf88bf12df
