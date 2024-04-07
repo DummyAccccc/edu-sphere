6463474d3af5b5e5ca05b346581e1a3a0dbbb06c
Aditya G <adityagawali044@gmail.com>
2024-04-07 10:58:38 +0530

    commit

--- client/src/Components/StudentTable.jsx
+++ client/src/Components/StudentTable.jsx
@@ -1,13 +1,48 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const StudentTable = (props) => {
 
     const data = props.data
     const navigate = useNavigate();
 
+    const user = useSelector(state => state.role);
+    const company = useSelector(state => state.company);
 
+    const Accept = () => {
+        toast.success(`Student Request Accepted! `, {
+            position: "top-right",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+        const myTimeout = setTimeout(() => (navigate('/dashboard')), 2000);
+    }
+
+    const Reject = () => {
+        toast.error('Student Request Rejected!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+        const myTimeout = setTimeout(() => (navigate('/dashboard')), 2000);
+
+    }
     const handleStudent = (id) => {
         navigate('/dashboard/github', {
             state: {
@@ -17,6 +52,19 @@ const StudentTable = (props) => {
     }
     return (
         <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition:Slide
+            />
             <div className="overflow-x-auto w-full border-2 text-white border-gray-700 rounded-lg px-8 py-8 bg-gray-700">
                 <h1 className='text-2xl py-2'>Student Table</h1>
                 <table className="table text-xl">
@@ -26,7 +74,7 @@ const StudentTable = (props) => {
                             <th>#</th>
                             <th>Name</th>
                             <th>Email</th>
-                            <th>Github</th>
+                            <th >Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -36,7 +84,8 @@ const StudentTable = (props) => {
                                 <th>{i + 1}</th>
                                 <td>{val.name}</td>
                                 <td>{val.email}</td>
-                                <td><button onClick={() => handleStudent(val.company)} className='btn btn-primary'>Github</button></td>
+                                <td>{user == "Instructor" ? <button onClick={() => handleStudent(val.company)} className='btn btn-primary'>Github</button> : <>
+                                    <button onClick={() => Accept()} className='btn btn-primary'>Accept</button> <button onClick={() => Reject(val.company)} className='btn btn-error'>Reject</button></>}</td>
                             </tr>
                         })}
 
